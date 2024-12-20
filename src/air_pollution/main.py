aaf00a9580b487c64f81231987d281d7f557277d
Mariam <mariam_hsein@hotmail.com>
2024-12-20 12:33:26 +0200

    add monitoring for inference

--- src/air_pollution/main.py
+++ src/air_pollution/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from loguru import logger
+from prometheus_fastapi_instrumentator import Instrumentator
 from air_pollution.endpoints.health import router as health_router
 from air_pollution.endpoints.train_pipeline import router as pipeline_router
+from air_pollution.endpoints.inference_pipeline import router as inference_router
 
 # Initialize the FastAPI app
 app = FastAPI(title="Air Pollution Project API", version="1.0")
@@ -8,8 +11,12 @@ app = FastAPI(title="Air Pollution Project API", version="1.0")
 # Include API routes
 app.include_router(health_router, prefix="/api", tags=["Health"])
 app.include_router(pipeline_router, prefix="/api", tags=["Train"])
-# app.include_router(inference_router, prefix="/api", tags=["Inference"])
+app.include_router(inference_router, prefix="/api", tags=["Inference"])
 
+# Add Prometheus metrics
+Instrumentator().instrument(app).expose(app)
+
+logger.add("logs/ml_service.log", rotation="10 MB")
 
 if __name__ == "__main__":
     import uvicorn
